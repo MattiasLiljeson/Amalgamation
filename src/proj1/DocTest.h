dba36d58447e066bbf7a1eeb4902672ffcab1bea
jarllarsson <jarl.larsson@gmail.com>
2012-11-14 17:19:51 +0100

    syntax update

--- src/proj1/DocTest.h
+++ src/proj1/DocTest.h
@@ -1,16 +1,34 @@
-/*! A test class */
+// =================================================================================================
+//                                         Test
+// =================================================================================================
+
+///-------------------------------------------------------------------------------------------------
+/// \brief Brief description, Lorem
+///        ipsum dolor sit amet
+/// 
+/// # ClassName
+/// Detailed description will then start here, describe everything
+/// using lots of fancy words.
+/// Edited By: Name, 2012-11-14
+///-------------------------------------------------------------------------------------------------
 class Test
 {
 public:
-    /** An enum type.
-    * The documentation block cannot be put after the enum!
-    */
+    ///
+	/// An enum type.
+    /// The documentation block cannot be put after the enum!
+    ///
     enum EnumType
     {
         int EVal1, /**< enum value 1 */
         int EVal2 /**< enum value 2 */
     };
-    void member(); //!< a member function.
+    void member(); ///< a member function.
+	///
+	/// Multiline for func, wip
+    ///
+	void anotherfunc();
 protected:
-    int value; /*!< an integer value */
+    int value; ///< an integer value 
+	           ///<
 };
