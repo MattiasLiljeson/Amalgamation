56a9769f4b3930ef895f4ffedb3893092c3549b4
Johan Carlberg <johan.carlsberg@gmail.com>
2012-11-29 16:59:59 +0100

    ThreadSafeMessaging implemented and documented. Can be added more features later (mostly performance related features since it is now using mutex-locks at EVERY push and pop to the message queue.)

--- Projects/NetComm/Src/ThreadSafeMessaging.h
+++ Projects/NetComm/Src/ThreadSafeMessaging.h
@@ -17,13 +17,32 @@ public:
 	ThreadSafeMessaging();
 	~ThreadSafeMessaging();
 
+	///-----------------------------------------------------------------------------------
+	/// Put a message on ThreadSafeMessaging's own message queue.
+	/// \param ProcessMessage * p_message
+	/// \returns void
+	///-----------------------------------------------------------------------------------
 	void putMessage( ProcessMessage* p_message );
 
 protected:
+	///-----------------------------------------------------------------------------------
+	/// Pop oldest message from the message queue.
+	/// \returns ProcessMessage*
+	///-----------------------------------------------------------------------------------
 	ProcessMessage* popMessage();
 
+	///-----------------------------------------------------------------------------------
+	/// Get the amount of messages currently stored in the message queue.
+	/// \returns unsigned int
+	///-----------------------------------------------------------------------------------
+	unsigned int getMessageCount();
+
 private:
-	queue< ProcessMessage* > m_messageQueue;
-	boost::mutex m_guard;
+	queue< ProcessMessage* > m_messageQueue; /**< Stores all messages that are "put"
+											on the ThreadSafeMessaging instance. Every
+											access to the queue must be thread safe. */
+
+	boost::mutex m_guard; /**< A unique lock that is used when accessing the message
+						  queue. */
 
 };
