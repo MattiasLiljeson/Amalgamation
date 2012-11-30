6a40342e7f0fed3f52ac64fa5ce254aa6f80cf39
Alexander Brodén <broden.alexander+github@gmail.com>
2012-11-30 18:05:35 +0100

    Added a basic loop processing world

--- Projects/Gtest/Src/EntityWorldTEST.cpp
+++ Projects/Gtest/Src/EntityWorldTEST.cpp
@@ -15,4 +15,12 @@ TEST(EntityWorldBasic, EntityWorldInstancingTest)
 
 	world->setSystem(SystemType::getTypeFor(SystemType::EntitySystem), system, true);
 	Entity* ent = world->createEntity();
+
+	float dt = 1.0f/60;
+
+	while (true)
+	{
+		world->setDelta(dt);
+		world->process();
+	}
 }
