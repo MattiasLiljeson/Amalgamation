6cd8b6fe6fa1638f5d4e456edf98b45a0c2f1653
Robin Thunström <robin.thunstrom@gmail.com>
2012-12-03 15:39:58 +0100

    Updated doxygen build files to work through all projects, separately as well as one combined.

--- BuildDocs.bat
+++ BuildDocs.bat
@@ -1,1 +1,11 @@
-doxygen DoxyfileProj1
+doxygen Doxyfile
+doxygen DoxyGraphics
+doxygen DoxyAssetsManagement
+doxygen DoxyAudio
+doxygen DoxyClientApp
+doxygen DoxyES
+doxygen DoxyGameplay
+doxygen DoxyInput
+doxygen DoxyNetComm
+doxygen DoxyPhysics
+doxygen DoxyServerApp
