a65c633a67dc3bf834c66e81d151421ec906728f
jarllarsson <jarl.larsson@gmail.com>
2012-12-05 15:11:35 +0100

    Changed name on resource map to resource manager as it is no longer a pure map structure.

--- Projects/AssetsManagement/Src/ResourceManager.h
+++ Projects/AssetsManagement/Src/ResourceManager.h
@@ -18,10 +18,10 @@
 using namespace std;
 
 template <class T>
-class ResourceMap
+class ResourceManager
 {
 public:
-		~ResourceMap() {clear();}
+		~ResourceManager() {clear();}
 
 		///-----------------------------------------------------------------------------------
 		/// Clear the entire resource map and all its data.
@@ -124,7 +124,7 @@ private:
 
 
 template <class T>
-void ResourceMap<T>::clear()
+void ResourceManager<T>::clear()
 {
 		typename map<string,T*>::iterator mapIter = m_resourceMap.begin();
 		for (; mapIter!=m_resourceMap.end(); mapIter++)
@@ -136,7 +136,7 @@ void ResourceMap<T>::clear()
 };
 
 template <class T>
-T* ResourceMap<T>::getResource(const string& p_uniqueName)
+T* ResourceManager<T>::getResource(const string& p_uniqueName)
 {
 		T* resourceObj = getResourceContainerFromName(p_uniqueName)->data;
 
@@ -144,37 +144,37 @@ T* ResourceMap<T>::getResource(const string& p_uniqueName)
 };
 
 template <class T>
-T* ResourceMap<T>::getResource(unsigned int p_uniqueId)
+T* ResourceManager<T>::getResource(unsigned int p_uniqueId)
 {
 	return m_resourceList[p_uniqueId]->data;
 };
 
 template <class T>
-T* ResourceMap<T>::operator[](unsigned int p_uniqueId)
+T* ResourceManager<T>::operator[](unsigned int p_uniqueId)
 {
 	return m_resourceList[p_uniqueId]->data;
 };
 
 template <class T>
-unsigned int ResourceMap<T>::getResourceId(const string& p_uniqueName)
+unsigned int ResourceManager<T>::getResourceId(const string& p_uniqueName)
 {
 	return getResourceContainerFromName(p_uniqueName)->uniqueId;
 };
 
 template <class T>
-const string& ResourceMap<T>::getResourceName(unsigned int p_uniqueId)
+const string& ResourceManager<T>::getResourceName(unsigned int p_uniqueId)
 {
 	return m_resourceList[p_uniqueId]->uniqueName;
 };
 
 template <class T>
-unsigned int ResourceMap<T>::addResource(const string& p_uniqueName, T* p_resourceObj)
+unsigned int ResourceManager<T>::addResource(const string& p_uniqueName, T* p_resourceObj)
 {
 		// do an insert and return either the inserted value or the already existing value
 		unsigned int reval = -1;
 
 		// Create empty container
-		ResourceMap::ResourceDataContainer* container = new ResourceDataContainer();
+		ResourceManager::ResourceDataContainer* container = new ResourceDataContainer();
 
 		// Try to make an insertion using the unique name as key
 		// if it already exists we get a pointer to the existing data
@@ -202,19 +202,19 @@ unsigned int ResourceMap<T>::addResource(const string& p_uniqueName, T* p_resour
 }
 
 template <class T>
-bool ResourceMap<T>::removeResource(const string& p_uniqueName)
+bool ResourceManager<T>::removeResource(const string& p_uniqueName)
 {
 	m_resourceMap.erase(p_uniqueName)==1?return true:return false;
 }
 
 template <class T>
-bool ResourceMap<T>::removeResource(unsigned int p_uniqueId)
+bool ResourceManager<T>::removeResource(unsigned int p_uniqueId)
 {
 	return m_resourceList.removeAt(p_uniqueId);
 }
 
 template <class T>
-typename ResourceMap<T>::ResourceDataContainer* ResourceMap<T>::getResourceContainerFromName(const string& p_uniqueName)
+typename ResourceManager<T>::ResourceDataContainer* ResourceManager<T>::getResourceContainerFromName(const string& p_uniqueName)
 {
 	ResourceDataContainer* resourceContainer = NULL;
 	typename map<string,ResourceDataContainer*>::iterator mapIter;
