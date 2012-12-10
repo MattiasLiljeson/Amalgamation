6455244316ab8567516464d9d8f19512ccf1ab2f
jarllarsson <jarl.larsson@gmail.com>
2012-12-10 15:04:54 +0100

    old sprint

---
+++ Projects/Graphics/Src/RendererMeshInfo.h
@@ -0,0 +1,23 @@
+#pragma once
+
+// =======================================================================================
+//                             RendererMeshInfo
+// =======================================================================================
+
+///---------------------------------------------------------------------------------------
+/// \brief	Interfacing struct for renderer. Information on what will be rendered.
+///        
+/// # RendererMeshInfo
+/// Detailed description.....
+/// Created on: 7-12-2012 
+///---------------------------------------------------------------------------------------
+
+#include "InstanceVertex.h"
+
+struct RendererMeshInfo
+{
+	unsigned int p_meshId;
+	unsigned int p_textureId;
+	InstanceVertex* p_instanceList;
+	unsigned int p_numberOfInstances;
+};
