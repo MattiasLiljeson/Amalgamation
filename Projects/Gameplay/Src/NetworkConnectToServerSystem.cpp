6743dec5d9381d41f535fef677a4a71140ddd9d1
Johan Carlberg <johan.carlsberg@gmail.com>
2012-12-14 15:53:38 +0100

    Misplaced code. Todo: add the code to another system.

--- Projects/Gameplay/Src/NetworkConnectToServerSystem.cpp
+++ Projects/Gameplay/Src/NetworkConnectToServerSystem.cpp
@@ -1,9 +1,11 @@
 #include "NetworkConnectToServerSystem.h"
 
-NetworkConnectToServerSystem::NetworkConnectToServerSystem( TcpClient* p_tcpClient )
+NetworkConnectToServerSystem::NetworkConnectToServerSystem( TcpClient* p_tcpClient,
+												InputBackendSystem* p_inputBackend  )
 	: EntitySystem( SystemType::NetworkConnectoToServerSystem )
 {
 	m_tcpClient = p_tcpClient;
+	m_inputBackend = p_inputBackend;
 }
 
 NetworkConnectToServerSystem::~NetworkConnectToServerSystem()
@@ -13,6 +15,9 @@ NetworkConnectToServerSystem::~NetworkConnectToServerSystem()
 
 void NetworkConnectToServerSystem::processEntities( const vector<Entity*>& p_entities )
 {
+	Control* keyL = m_inputBackend->getInputControl("L");
+	double pressness = keyL->getDelta(); //pressed = 1.0
+
 	if( !m_tcpClient->hasActiveConnection() )
 	{
 		m_tcpClient->connectToServer( "127.0.0.1", "1337" );
