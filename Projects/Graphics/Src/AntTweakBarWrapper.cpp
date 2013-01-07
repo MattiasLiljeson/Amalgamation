a139bc5d0a33c88013b11a14b7342e31d35be705
Robin Thunström <robin.thunstrom@gmail.com>
2013-01-07 16:06:21 +0100

    Added tons of stuff to Ant Tweak Bar, mostly revolving around multiple bars.

--- Projects/Graphics/Src/AntTweakBarWrapper.cpp
+++ Projects/Graphics/Src/AntTweakBarWrapper.cpp
@@ -1,21 +1,39 @@
 #include "AntTweakBarWrapper.h"
+#include <ToString.h>
 
 AntTweakBarWrapper* AntTweakBarWrapper::sInstance = NULL;
 
 AntTweakBarWrapper::AntTweakBarWrapper( ID3D11Device* p_device)
 {
 	TwInit(TW_DIRECT3D11,p_device);
+
 	m_overallBar	= TwNewBar(overall.c_str());
 	m_soundBar		= TwNewBar(sound.c_str());
 	m_graphicsBar	= TwNewBar(graphics.c_str());
 	m_input			= TwNewBar(input.c_str());
 	m_network		= TwNewBar(network.c_str());
+
+	initBars();
 }
 
 AntTweakBarWrapper::~AntTweakBarWrapper()
 {
 	TwTerminate();
 }
+
+void AntTweakBarWrapper::initBars()
+{
+	TwDefine(" GLOBAL contained=true ");
+	TwDefine(" GLOBAL fontresizable=false ");
+	defineParametersForABar(overall.c_str()," color='109 158 235'");
+	defineParametersForABar(sound.c_str()," color='142 124 195' iconified=true");
+	defineParametersForABar(graphics.c_str()," color='255 217 102' iconified=true");
+	defineParametersForABar(input.c_str()," color='224 102 102' iconified=true");
+	defineParametersForABar(network.c_str()," color='147 196 125' iconified=true");
+
+	modifyTheRefreshRate(INPUT,0.1f);
+}
+
 void AntTweakBarWrapper::render()
 {
 	TwDraw();
@@ -42,7 +60,7 @@ void AntTweakBarWrapper::handleMessage( HWND hWnd, UINT message, WPARAM wParam,
 		return; // event has been handled by AntTweakBar
 }
 
-void AntTweakBarWrapper::addReadOnlyVariable(Bar p_barType, const char* p_name, 
+void AntTweakBarWrapper::addReadOnlyVariable(BarType p_barType, const char* p_name, 
 											 TwType p_type, const void *p_var, 
 											 const char* p_misc)
 {
@@ -66,7 +84,7 @@ void AntTweakBarWrapper::addReadOnlyVariable(Bar p_barType, const char* p_name,
 	}
 }
 
-void AntTweakBarWrapper::addWriteVariable(Bar p_barType, const char* p_name, 
+void AntTweakBarWrapper::addWriteVariable(BarType p_barType, const char* p_name, 
 										  TwType p_type, void *p_var, 
 										  const char* p_misc)
 {
@@ -90,7 +108,7 @@ void AntTweakBarWrapper::addWriteVariable(Bar p_barType, const char* p_name,
 	}
 }
 
-TwBar* AntTweakBarWrapper::getAntBar(Bar p_barType)
+TwBar* AntTweakBarWrapper::getAntBar(BarType p_barType)
 {
 	TwBar* searchedBar = NULL;
 	
@@ -114,4 +132,37 @@ TwBar* AntTweakBarWrapper::getAntBar(Bar p_barType)
 	}
 
 	return searchedBar;
+}
+
+void AntTweakBarWrapper::defineParametersForABar( const char* p_nameOfBar, 
+												 const char* p_params )
+{
+	string tmp = " ";
+	tmp += p_nameOfBar;
+	tmp += p_params;
+	TwDefine(tmp.c_str());
+}
+
+void AntTweakBarWrapper::modifyTheRefreshRate(BarType p_barType, float p_value)
+{
+	string tmp = " refresh=";
+	tmp += toString(p_value);
+	switch (p_barType)
+	{
+	case AntTweakBarWrapper::SOUND:
+		defineParametersForABar(sound.c_str(),tmp.c_str());
+		break;
+	case AntTweakBarWrapper::GRAPHICS:
+		defineParametersForABar(graphics.c_str(),tmp.c_str());
+		break;
+	case AntTweakBarWrapper::INPUT:
+		defineParametersForABar(input.c_str(),tmp.c_str());
+		break;
+	case AntTweakBarWrapper::NETWORK:
+		defineParametersForABar(network.c_str(),tmp.c_str());
+		break;
+	default:
+		defineParametersForABar(overall.c_str(),tmp.c_str());
+		break;
+	}
 }
--- Projects/Graphics/Src/AntTweakBarWrapper.h
+++ Projects/Graphics/Src/AntTweakBarWrapper.h
@@ -4,6 +4,7 @@
 #include <string>
 
 using namespace std;
+
 namespace AntTweakBarStrings{
 	static const string& overall	= "Overall";
 	static const string& sound		= "Sound";
@@ -11,6 +12,7 @@ namespace AntTweakBarStrings{
 	static const string& input		= "Input";
 	static const string& network	= "Network";
 }
+
 using namespace AntTweakBarStrings;
 // =======================================================================================
 //                                      AntTweakBar
@@ -29,7 +31,7 @@ using namespace AntTweakBarStrings;
 class AntTweakBarWrapper
 {
 public:
-	enum Bar
+	enum BarType
 	{
 		OVERALL, SOUND, GRAPHICS, INPUT, NETWORK
 	};
@@ -38,7 +40,6 @@ public:
 	/// This function should only be called with parameters once to init Ant Tweak Bar 
 	/// Wrapper(ATBW) properly. Afterwards just call the function without any parameters
 	/// \param p_device
-	/// \param p_nameOfBar
 	/// \return AntTweakBarWrapper*
 	///-----------------------------------------------------------------------------------
 	static AntTweakBarWrapper* getInstance(ID3D11Device* p_device=NULL);
@@ -49,18 +50,86 @@ public:
 	///-----------------------------------------------------------------------------------
 	static void destroy();
 
-	void	addReadOnlyVariable(Bar p_barType, const char* p_name, TwType p_type, 
+	///-----------------------------------------------------------------------------------
+	/// Takes a variable and maps it to the specified bar allowing for AntTweakBar to only
+	/// read it. 
+	/// \param p_barType
+	/// \param p_name
+	/// \param p_type
+	/// \param p_var
+	/// \param p_misc
+	/// \return void
+	///-----------------------------------------------------------------------------------
+	void addReadOnlyVariable(BarType p_barType, const char* p_name, TwType p_type, 
 		const void *p_var, const char* p_misc);
 
-	void	addWriteVariable(Bar p_barType, const char* p_name, TwType p_type, void *p_var,
+	///-----------------------------------------------------------------------------------
+	/// Takes a variable and maps it to the specified bar allowing for AntTweakBar to read
+	/// and write to it. 
+	/// \param p_barType
+	/// \param p_name
+	/// \param p_type
+	/// \param p_var
+	/// \param p_misc
+	/// \return void
+	///-----------------------------------------------------------------------------------
+	void addWriteVariable(BarType p_barType, const char* p_name, TwType p_type, void *p_var,
 		const char* p_misc);
 
-	void	render();
-	void	handleMessage(HWND wnd, UINT msg, WPARAM wParam, LPARAM lParam);
-	TwBar*	getAntBar(Bar p_barType);
+	///-----------------------------------------------------------------------------------
+	/// Tells AntTweakBar to be rendered
+	/// \return void
+	///-----------------------------------------------------------------------------------
+	void render();
+
+	///-----------------------------------------------------------------------------------
+	/// Receives the messages sent from the windows message loop
+	/// \param wnd
+	/// \param msg
+	/// \param wParam
+	/// \param lParam
+	/// \return void
+	///-----------------------------------------------------------------------------------
+	void handleMessage(HWND wnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+	///-----------------------------------------------------------------------------------
+	/// Returns the specified AntTweakBar
+	/// \param p_barType
+	/// \return TwBar*
+	///-----------------------------------------------------------------------------------
+	TwBar* getAntBar(BarType p_barType);
+
+	///-----------------------------------------------------------------------------------
+	/// Allows for modification on the refresh-rate for any Bar, negative values are not
+	/// a valid input.
+	/// \param p_barType
+	/// \param p_value
+	/// \return void
+	///-----------------------------------------------------------------------------------
+	void modifyTheRefreshRate(BarType p_barType, float p_value);
+
 private:
+	///-----------------------------------------------------------------------------------
+	/// The constructor is private since ATBW is a singleton class
+	/// \param p_device
+	/// \return 
+	///-----------------------------------------------------------------------------------
 	AntTweakBarWrapper(ID3D11Device* p_device);
 	virtual ~AntTweakBarWrapper();
+
+	///-----------------------------------------------------------------------------------
+	/// Initialize the values and parameters for all of the Bars.
+	/// \return void
+	///-----------------------------------------------------------------------------------
+	void initBars();
+
+	///-----------------------------------------------------------------------------------
+	/// A help method that allows for easy addition of defines for a given Bar.
+	/// \param p_nameOfBar
+	/// \param p_params
+	/// \return void
+	///-----------------------------------------------------------------------------------
+	void defineParametersForABar(const char* p_nameOfBar, const char* p_params);
 private:
 	TwBar* m_overallBar;
 	TwBar* m_soundBar;
