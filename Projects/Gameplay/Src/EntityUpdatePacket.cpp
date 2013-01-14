0f771c5bdc8751275b95a1f56c9eea3c5ee1afcb
Robin Thunström <robin.thunstrom@gmail.com>
2013-01-14 14:24:32 +0100

    Work in progress.

---
+++ Projects/Gameplay/Src/EntityUpdatePacket.cpp
@@ -0,0 +1,37 @@
+#include "EntityUpdatePacket.h"
+#include "PacketType.h"
+
+EntityUpdatePacket::EntityUpdatePacket()
+{
+	entityType = 'x';
+	networkIdentity = -1;
+	translation = AglVector3();
+	rotation	= AglQuaternion();
+	scale		= AglVector3();
+}
+
+EntityUpdatePacket::~EntityUpdatePacket()
+{
+
+}
+
+Packet EntityUpdatePacket::pack()
+{
+	Packet packet(static_cast<char>(PacketType::EntityUpdate));	
+	packet << entityType
+		<< networkIdentity
+		<< translation
+		<< rotation
+		<< scale;
+
+	return packet;
+}
+
+void EntityUpdatePacket::unpack( Packet p_packet )
+{
+	p_packet >> entityType
+		>> networkIdentity 
+		>> translation 
+		>> rotation 
+		>> scale;
+}
