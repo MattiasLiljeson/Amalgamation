435e02a095d9955f407629265180410f38359854
BiceMaster <mattiasliljeson+cecilia-pc@gmail.com>
2013-01-14 17:27:14 +0100

    Assamblages and it's classes are now documented through doxygen

--- Projects/ES/Src/ComponentData.h
+++ Projects/ES/Src/ComponentData.h
@@ -6,6 +6,28 @@
 #include<sstream>
 using namespace std;
 
+// =======================================================================================
+//                                      ComponentData
+// =======================================================================================
+
+///---------------------------------------------------------------------------------------
+/// \brief Data container struct. Used to transmit data between assemblage files and 
+/// Constructed components.
+///
+///	\author		Mattias Liljeson
+///
+/// # ComponentData
+/// This struct is used to transfer data from Assemblages to the actual constructed 
+/// components. This data is stored in avoid pointer array so that all possible data can
+/// be stored. Out of the box, support for basic data types and strings are supported
+/// where strings need to be handled through a special case due to their internal 
+/// structure. Feel free to add support for any datatype you'll like.
+///
+/// There's no destructor as that would cause a lot of heap allocation and copy operations
+/// to occur as you then would need to have a copy constructor. Use release() when you're 
+/// finished working with the data instead.
+/// Created on: 14-1-2013 
+///---------------------------------------------------------------------------------------
 struct ComponentData
 {
 	char dataType;
@@ -16,19 +38,69 @@ struct ComponentData
 	ComponentData();
 	void release();
 
+	///-----------------------------------------------------------------------------------
+	/// Setter for basic data types. No error checking for faulty lines etc done. The type
+	/// supplied must be allocated on the stack as memcpy is used to save the data.
+	/// \param p_ss The stream used to parse type, name and value.
+	/// \return void
+	///-----------------------------------------------------------------------------------
 	template<typename T>
 	void setData( stringstream* p_ss );
 	
+	///-----------------------------------------------------------------------------------
+	/// Get the data as the supplied type. Does no type checking whatsoever as 
+	/// reinterpret_cast is used.
+	/// \param the pointer that will be dereferenced and set to the read data.
+	/// \return AssemblageHelper::E_FileStatus If the out parameter is null,
+	/// FileStatus_COMPONENT_DATA_CONVERSION_FAILED will be returned. Otherwise
+	/// FileStatus_OK will be returned
+	///-----------------------------------------------------------------------------------
 	template<typename T>
 	AssemblageHelper::E_FileStatus getData( T* out_value );
 	
-	// NOTE: Will never return conversion failed as raw memcpy is used. No type 
-	// checking is done! Only returns if the variable wasn't defined.
+	///-----------------------------------------------------------------------------------
+	/// Special case function used in conjunction with strings as they store characters on
+	/// the heap. Can handle multi word strings.
+	///
+	/// NOTE: Will never return conversion failed as raw memcpy is used. No type 
+	/// checking is done! 
+	/// \param p_ss The stream used to parse type, name and value.
+	/// \return AssemblageHelper::E_FileStatus returns FileStatus_OK if everything is 
+	/// fine. Returns FileStatus_COMPONENT_DATA_CONVERSION_FAILED if the variable wasn't 
+	///defined in the assemblage.
+	///-----------------------------------------------------------------------------------
 	AssemblageHelper::E_FileStatus setDataAsString( stringstream* p_ss );
+
+
+	///-----------------------------------------------------------------------------------
+	/// Get the data as a string. Does no type checking whatsoever as reinterpret_cast is
+	/// used.
+	/// \param out_data The pointer that will be dereferenced and set to the read data.
+	/// \return AssemblageHelper::E_FileStatus If the out parameter is null, 
+	/// FileStatus_COMPONENT_DATA_CONVERSION_FAILED will be returned. Otherwise
+	/// FileStatus_OK will be returned
+	///-----------------------------------------------------------------------------------
 	AssemblageHelper::E_FileStatus getDataAsString( string* out_data );
 
+	///-----------------------------------------------------------------------------------
+	/// Manually set contents of this struct as char array.
+	/// \param p_dataType A char the symbolizes the data type. Should be 'a' as in array. 
+	/// Use other char if you store other information than chars or if you want to parse
+	/// the contents in a special way
+	/// \param p_dataName Name of the data/variable
+	/// \param p_src Where to start copying from
+	/// \param p_size size of c string / number of bytes to copy.
+	/// \return void
+	///-----------------------------------------------------------------------------------
 	void setDataAsCharArray( char p_dataType, string p_dataName, const char* p_src, int p_size );
-	void getDataAsCharArray( char** out_data );
+
+	///-----------------------------------------------------------------------------------
+	/// Fetch data as a c string.
+	/// \param pointer to char pointer which will be set to point to the internal data of
+	/// this instance. Will be destroyed when release() is called. 
+	/// \return void
+	///-----------------------------------------------------------------------------------
+	void getDataAsCharArray( char** out_data);
 };
 
 template<typename T>
--- Projects/ES/Src/EntityFactory.h
+++ Projects/ES/Src/EntityFactory.h
@@ -12,14 +12,42 @@ class AssemblageReader;
 
 using namespace std;
 
+// =======================================================================================
+//                                      EntityFactory
+// =======================================================================================
+
+///---------------------------------------------------------------------------------------
+/// \brief This is a native ES EntitySystem subclass which creates Entities from recipes 
+/// which are loaded from assemblage definition files (.asd). 
+///        
+///	\author		Mattias Liljeson
+/// 
+/// Created on: 14-1-2013 
+///---------------------------------------------------------------------------------------
+
 class EntityFactory : public EntitySystem
 {
 public:
 	EntityFactory();
 	~EntityFactory();
 
+	///-----------------------------------------------------------------------------------
+	/// Read assemblage definition file and create a recipe from it which will be stored
+	/// locally and used when queried for an Entity. If a recipe already exists it will be
+	/// replaced by this new one.
+	/// \param p_filePath Path to the assemblage definition file.
+	/// \return AssemblageHelper::E_FileStatus Status of the operation. If the assemblage
+	/// was loaded successfully this function will return
+	///-----------------------------------------------------------------------------------
 	AssemblageHelper::E_FileStatus readAssemblageFile( string p_filePath );
 
+	///-----------------------------------------------------------------------------------
+	/// Create an entity by supplying recipe/entity name.
+	/// \param p_entityName Name of the entity.
+	/// \return Entity* An entity which has been created by the world but not added to it.
+	/// This is so that you can make last minute changes to the components and their data
+	/// before letting it out in the wild. NULL if the requested recipe isn't found.
+	///-----------------------------------------------------------------------------------
 	Entity* entityFromRecipe( const string& p_entityName );
 
 private:
