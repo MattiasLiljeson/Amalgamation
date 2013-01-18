824e8d5ea745ec5d93b51d69346b28fed6d2e647
Johan Carlberg <johan.carlsberg@gmail.com>
2013-01-18 10:32:55 +0100

    Added an AntTweakBar button for toggling the use of extrapolation. Wrote some documentation in ExtrapolationSystem.

--- Projects/Gameplay/Src/ExtrapolationSystem.cpp
+++ Projects/Gameplay/Src/ExtrapolationSystem.cpp
@@ -6,12 +6,16 @@
 #include "Extrapolate.h"
 #include "Transform.h"
 
+ExtrapolationSystem* ExtrapolationSystem::m_selfPointer = NULL;
+
 ExtrapolationSystem::ExtrapolationSystem( TcpClient* p_client )
 	: EntitySystem( SystemType::ExtrapolationSystem, 2,
 	ComponentType::Extrapolate, ComponentType::Transform )
 {
 	m_client = p_client;
 	m_correctedDeltaTime = 0;
+	m_usePositionalExtrapolation = 0;
+	m_selfPointer = this;
 }
 
 ExtrapolationSystem::~ExtrapolationSystem()
@@ -21,33 +25,32 @@ ExtrapolationSystem::~ExtrapolationSystem()
 void ExtrapolationSystem::processEntities( const vector<Entity*>& p_entities )
 {
 	float latestUpdateTimeStamp = searchForLatestUpdateTimeStamp( p_entities );
-	unsigned int numEntities = p_entities.size();
 
 	for( unsigned int i=0; i<p_entities.size(); i++ )
 	{
 		Extrapolate* extrapolate = NULL;
 		extrapolate = static_cast<Extrapolate*>(p_entities[i]->getComponent(
 			ComponentType::Extrapolate));
-
 		Transform* transform = NULL;
 		transform = static_cast<Transform*>(p_entities[i]->getComponent(
 			ComponentType::Transform));
-
 		AglVector3 translation = transform->getTranslation();
 		AglVector3 velocity = extrapolate->velocityVector;
 		AglVector3 angularVelocity = extrapolate->angularVelocity;
 
-//		float packetUpdateTime = extrapolate->serverUpdateTimeStamp;
-//		m_correctedDeltaTime = latestUpdateTimeStamp - packetUpdateTime;
-
 		m_correctedDeltaTime = latestUpdateTimeStamp -
 			extrapolate->serverUpdateTimeStamp;
 
-		// Extrapolate translation
-		translation += (velocity * m_correctedDeltaTime);
-		transform->setTranslation( translation );
+		if(m_usePositionalExtrapolation)
+		{
+			// Extrapolate translation
+			translation += (velocity * m_correctedDeltaTime);
+			transform->setTranslation( translation );
+		}
 
-//		// Extrapolate orientation (Must be fixed because it rotates modules locally.)
+		// NOTE (Johan): Extrapolate orientation currently rotates locally (but should
+		// rotate with the ship.
+		// NOTE (Johan): Orientation extrapolation is disabled until velocity is fixed!
 //		AglQuaternion orientation = transform->getRotation();
 //		AglQuaternion velocityQuaternion = AglQuaternion::constructFromAngularVelocity(
 //			angularVelocity * m_correctedDeltaTime );
@@ -62,6 +65,9 @@ void ExtrapolationSystem::initialize()
 	AntTweakBarWrapper::getInstance()->addReadOnlyVariable(
 		AntTweakBarWrapper::NETWORK, "Corrected delta",
 		TwType::TW_TYPE_FLOAT, &m_correctedDeltaTime, "" );
+
+	TwAddButton(AntTweakBarWrapper::getInstance()->getAntBar(AntTweakBarWrapper::NETWORK),
+		"Toggle pos extrapolation", togglePositionalExtrapolation, (void*)NULL, "");
 }
 
 float ExtrapolationSystem::searchForLatestUpdateTimeStamp( const vector<Entity*>& p_entities )
@@ -80,4 +86,10 @@ float ExtrapolationSystem::searchForLatestUpdateTimeStamp( const vector<Entity*>
 	}
 
 	return latestUpdateTimeStamp;
+}
+
+void TW_CALL ExtrapolationSystem::togglePositionalExtrapolation( void* p_clientData )
+{
+	m_selfPointer->m_usePositionalExtrapolation =
+		!m_selfPointer->m_usePositionalExtrapolation;
 }
--- Projects/Gameplay/Src/ExtrapolationSystem.h
+++ Projects/Gameplay/Src/ExtrapolationSystem.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <AntTweakBarWrapper.h>
 #include <EntitySystem.h>
 class TcpClient;
 
@@ -8,10 +9,13 @@ class TcpClient;
 // =======================================================================================
 
 ///---------------------------------------------------------------------------------------
-/// \brief Brief description...
-///        
+/// \brief A system that is supposed to extrapolate the position of entities with the
+/// Extrapolate component.
+///
 /// # ExtrapolationSystem
-/// Detailed description...
+/// First synchronizing the older positional updates with the latest update so that all
+/// entities have the same local update time stamp. The difference in update time is also
+/// stored which the extrapolation uses.
 /// Created on: 15-1-2013 
 ///---------------------------------------------------------------------------------------
 
@@ -25,9 +29,20 @@ public:
 	void processEntities( const vector<Entity*>& p_entities );
 
 	void initialize();
+	///-----------------------------------------------------------------------------------
+	/// Find the latest update packet time stamp that can be compared with every entity to
+	/// be extrapolated.
+	/// \param p_entities
+	/// \return float
+	///-----------------------------------------------------------------------------------
 	float searchForLatestUpdateTimeStamp( const vector<Entity*>& p_entities );
+private:
+	static void TW_CALL togglePositionalExtrapolation(void* p_clientData);
+
 private:
 	TcpClient* m_client;
 	float m_correctedDeltaTime;
+	bool m_usePositionalExtrapolation;
+	static ExtrapolationSystem* m_selfPointer;
 
 };
