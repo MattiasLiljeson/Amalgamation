80c13451e05effff30ee2c865cb65949ae348da0
jarllarsson <jarl.larsson@gmail.com>
2013-01-21 17:10:20 +0100

    include path fixes

--- Projects/Gameplay/Src/ClientPacketHandlerSystem.cpp
+++ Projects/Gameplay/Src/ClientPacketHandlerSystem.cpp
@@ -33,6 +33,7 @@
 
 // Debug
 #include <DebugUtil.h>
+#include <ToString.h>
 #include "ShipEditController.h"
 #include "ConnectionPointSet.h"
 #include "TimerSystem.h"
@@ -43,7 +44,6 @@
 #include "WelcomePacket.h"
 #include "UpdateClientStatsPacket.h"
 #include "Extrapolate.h"
-#include "..\..\PhysicsTest\src\Utility.h"
 #include "InputBackendSystem.h"
 
 ClientPacketHandlerSystem::ClientPacketHandlerSystem( TcpClient* p_tcpClient )
@@ -483,11 +483,11 @@ void ClientPacketHandlerSystem::updateInitialPacketLossDebugData()
 		{
 			DEBUGPRINT(( string(
 				/* 0 - 511 */
-				ToString(0) + " - " +
-				ToString(511) +
+				toString(0) + " - " +
+				toString(511) +
 				/* byte size */
 				" = " +
-				ToString(511 * 51) +
+				toString(511 * 51) +
 				" bytes" +
 				/* end */
 				"\n").c_str() ));
@@ -500,11 +500,11 @@ void ClientPacketHandlerSystem::updateInitialPacketLossDebugData()
 		{
 			DEBUGPRINT(( string(
 				/* 0 - x */
-				ToString(0) + " - " +
-				ToString(m_staticPropIdentities.front()) +
+				toString(0) + " - " +
+				toString(m_staticPropIdentities.front()) +
 				/* byte size */
 				" = " +
-				ToString((m_staticPropIdentities.front() + 1) * 51) +
+				toString((m_staticPropIdentities.front() + 1) * 51) +
 				" bytes" +
 				/* end */
 				"\n").c_str() ));
@@ -523,11 +523,11 @@ void ClientPacketHandlerSystem::updateInitialPacketLossDebugData()
 			{
 				DEBUGPRINT(( string(
 					/* x - y */
-					ToString(firstValue) + " - " +
-					ToString(secondValue - 1) +
+					toString(firstValue) + " - " +
+					toString(secondValue - 1) +
 					/* byte size */
 					" = " +
-					ToString((secondValue - firstValue) * 51) +
+					toString((secondValue - firstValue) * 51) +
 					" bytes" +
 					/* end */
 					"\n").c_str() ));
@@ -543,11 +543,11 @@ void ClientPacketHandlerSystem::updateInitialPacketLossDebugData()
 			{
 				DEBUGPRINT(( string(
 					/* x - 511 */
-					ToString(m_staticPropIdentities.front()) + " - " +
-					ToString(511) +
+					toString(m_staticPropIdentities.front()) + " - " +
+					toString(511) +
 					/* byte size */
 					" = " +
-					ToString((511 - m_staticPropIdentities.front()) * 51) +
+					toString((511 - m_staticPropIdentities.front()) * 51) +
 					" bytes" +
 					/* end */
 					"\n").c_str() ));
@@ -563,13 +563,13 @@ void ClientPacketHandlerSystem::updateInitialPacketLossDebugData()
 		{
 			AntTweakBarWrapper::getInstance()->addReadOnlyVariable(
 				AntTweakBarWrapper::NETWORK,
-				("min" + ToString(i)).c_str(), TwType::TW_TYPE_INT32,
+				("min" + toString(i)).c_str(), TwType::TW_TYPE_INT32,
 				&m_staticPropIdentitiesForAntTweakBar[i].first,
 				"group='Missing packets range'" );
 
 			AntTweakBarWrapper::getInstance()->addReadOnlyVariable(
 				AntTweakBarWrapper::NETWORK,
-				("max" + ToString(i)).c_str(), TwType::TW_TYPE_INT32,
+				("max" + toString(i)).c_str(), TwType::TW_TYPE_INT32,
 				&m_staticPropIdentitiesForAntTweakBar[i].second,
 				"group='Missing packets range'" );
 		}
