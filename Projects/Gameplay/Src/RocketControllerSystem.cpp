3a8d9442eecfdd1a1b7911c39db7a9113d1a5483
Johan Carlberg <johan.carlsberg@gmail.com>
2013-01-31 13:59:34 +0100

    Applied damage on Missile collision, so that modules get dropped if hit by a missile.

--- Projects/Gameplay/Src/RocketControllerSystem.cpp
+++ Projects/Gameplay/Src/RocketControllerSystem.cpp
@@ -5,6 +5,7 @@
 #include "PhysicsSystem.h"
 #include <PhysicsController.h>
 #include "SpawnSoundEffectPacket.h"
+#include "ShipModule.h"
 
 RocketControllerSystem::RocketControllerSystem(TcpServer* p_server)
 	: EntitySystem(SystemType::RocketControllerSystem, 1, ComponentType::StandardRocket)
@@ -85,6 +86,18 @@ void RocketControllerSystem::processEntities(const vector<Entity*>& p_entities)
 			{
 				ps->getController()->ApplyExternalImpulse(body->GetWorld().GetTranslation(), 20, 20);
 				ps->getController()->InactivateBody(pb->m_id);
+
+				// Apply damage to first found collision
+				Entity* hitEntity = ps->getEntity(cols[0]);
+				if(hitEntity)
+				{
+					ShipModule* hitModule = static_cast<ShipModule*>(hitEntity->getComponent(
+						ComponentType::ShipModule));
+					if(hitModule)
+					{
+						hitModule->m_health = 0;
+					}
+				}
 
 				//Send an explosion sound effect
 				Transform* t = static_cast<Transform*>(p_entities[i]->getComponent(ComponentType::Transform));
