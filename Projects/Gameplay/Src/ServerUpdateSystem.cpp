3e15a11ee7e5e5b5a88cdabae30785f2ae27517d
Johan Carlberg <johan.carlsberg@gmail.com>
2013-02-05 16:03:56 +0100

    32ms sleep.

--- Projects/Gameplay/Src/ServerUpdateSystem.cpp
+++ Projects/Gameplay/Src/ServerUpdateSystem.cpp
@@ -119,7 +119,7 @@ void ServerUpdateSystem::processEntities( const vector<Entity*>& p_entities )
 	// packet handling is too slow when running Debug build otherwise.
 	TimerIntervals::Enum entityupdateInterval = TimerIntervals::Every8Millisecond;
 #ifdef _DEBUG
-	entityupdateInterval = TimerIntervals::Every8Millisecond;
+	entityupdateInterval = TimerIntervals::Every32Millisecond;
 #endif
 	if( timerSys->checkTimeInterval(entityupdateInterval) )
 	{
