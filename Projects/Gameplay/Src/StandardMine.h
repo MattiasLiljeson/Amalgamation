aa818d8ce2330c373418da20cf8fe226e913401d
Johan Carlberg <johan.carlsberg@gmail.com>
2013-02-14 16:00:07 +0100

    Refactoring of some wrongly used ES functionality in ShipManagerSystem (it was trying to do ES' job). Implemented an impulse to mines that are near a ship. Impulse is stronger the closer a ship is. Currently the mine has a "maximum range" of 80.0f units, but can be set in runtime.

--- Projects/Gameplay/Src/StandardMine.h
+++ Projects/Gameplay/Src/StandardMine.h
@@ -1,25 +1,14 @@
 #pragma once
-
 #include <Component.h>
-
-// =======================================================================================
-//	MineLayerModule
-// =======================================================================================
-
-///---------------------------------------------------------------------------------------
-/// \brief Describes a module that deploys mines
-///        
-/// # PlayerScore
-/// Detailed description...
-/// Created on: 4-1-2013 
-///---------------------------------------------------------------------------------------
 
 class StandardMine: public Component
 {
 public:
-	float m_age;
 	StandardMine();
 	~StandardMine();
-private:
+
+public:
+	float m_age;
+	float range;
 
 };
