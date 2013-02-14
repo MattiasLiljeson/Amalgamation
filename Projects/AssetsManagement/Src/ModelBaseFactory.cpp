88ea37198e337f61aa55e48b905971fad482e67e
Mattias Liljeson <mattiasliljeson+github@gmail.com>
2013-02-14 10:15:07 +0100

    Quick fix where the game crashed when a particle system without a texture was loaded. These are now ignored instead.

--- Projects/AssetsManagement/Src/ModelBaseFactory.cpp
+++ Projects/AssetsManagement/Src/ModelBaseFactory.cpp
@@ -419,13 +419,16 @@ void ModelBaseFactory::readAndStoreParticleSystems( SourceData& p_source,
 	for (unsigned int n=0;n<particleSystems;n++)
 	{
 		AglParticleSystem* ps = p_source.scene->getParticleSystem(n);
-		ParticleSystemInstruction psIntr;
-		psIntr.particleSystem = *ps;
-		psIntr.textureFileName = p_source.scene->getName(ps->getHeader().textureNameIndex,
-														 true);
-		if (p_source.modelNumber==0) // add support for particle parent?
+		if( ps->getHeader().textureNameIndex != -1 ) // If the particle effect has a texture
 		{
-			p_model->particleSystems.m_collection.push_back(psIntr);
+			ParticleSystemInstruction psIntr;
+			psIntr.particleSystem = *ps;
+			psIntr.textureFileName = p_source.scene->getName(ps->getHeader().textureNameIndex,
+															 true);
+			if (p_source.modelNumber==0) // add support for particle parent?
+			{
+				p_model->particleSystems.m_collection.push_back(psIntr);
+			}
 		}
 	}
 }
