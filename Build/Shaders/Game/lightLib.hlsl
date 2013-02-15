17787103f972800f84d2dfb82e2f2c042b7fd5b2
Per Fornander <per_fornander11@hotmail.com>
2013-02-15 14:53:35 +0100

    Cleaned up and manage to get a pretty similar light to blender

--- Build/Shaders/Game/lightLib.hlsl
+++ Build/Shaders/Game/lightLib.hlsl
@@ -76,76 +76,36 @@ float3 parallelLight( SurfaceInfo surface, LightInfo light, float3 eyePos, float
 		litColor += diffuseFactor * surface.diffuse.xyz * light.diffuse.xyz;
 		litColor += specFactor * surface.specular.xyz * light.specular.xyz;
 	}
-	return float3(1,1,1);
+	return float3(0,0,0);
 	return litColor;
 }
 
 float3 pointLight( SurfaceInfo surface, LightInfo light, float3 eyePos, float3 normal, float3 pixelPos )
 {	
-
-
-	//return surface.diffuse.xyz;
-
-	// lulz tonemapping
-	//surface.diffuse *=  float4( 1.1, 0.8, 0.5, 1.0f );
-	
-	// The vector from the surface to the light.
 	float3 lightVec = light.pos - pixelPos;
-	
-	// The distance from surface to light.
 	float dist = length( lightVec );
-	
-	if( dist > light.range )
-	{
-		//return float3(0,0.2,0);
-		//return float3(0.0026,0.00035,0.0021);
-		return float3(0,0,0);
-		//return light.ambient.xyz;
-		//float3 green = float3(0.0f, 1.0f, 0.0f);
-		//return green;
-	}
-	// Normalize the light vector.
 	lightVec /= dist;
-	
-	// Add the ambient light term.
+	float range = light.range*0.5;
+	float3 eyeVec = eyePos - pixelPos;
+	float energy = light.spotPower;
+
 	float3 litColor = float3( 0.0f, 0.0f, 0.0f );
-
-	//litColor += surface.diffuse.xyz * light.ambient.xyz;
-	//litColor += surface.diffuse.xyz * float3(0.013,0.00035,0.010);
-
-	// Add diffuse and specular term, provided the surface is in
-	// the line of site of the light.
-
-
-	//float attenuation = 1.0f / light.range;
-	//attenuation = attenuation * pow(clampedCosine, 
-	
-	//attenuation = 1.0f / (1.0f +  light.attenuation.y*dist+ light.attenuation.z*dist*dist)*2;
-	float diffuseFactor = dot( lightVec, normal );
-	[branch]
-	if( diffuseFactor > 0.0f )
+	if(dist > range*2.5)
 	{
-		//return float3(0, 0, 0.5);
-		float specPower = max( surface.specular.a, 1.0f );
-		float3 toEye = normalize( eyePos - pixelPos );
-		float3 R = reflect( -lightVec, normal );
-		float specFactor = pow( max( dot(R, toEye), 0.0f ), specPower );
-		float attenuation;
-		attenuation = ((light.range*light.range / ( light.range*light.range + light.attenuation.z*R*R)));
-		//float lightIntensity
-		// diffuse and specular terms
-		litColor += saturate(diffuseFactor * (surface.diffuse.rgb * light.diffuse.rgb* attenuation));
-		litColor += saturate(specFactor * (surface.specular.rgb * light.specular.rgb* attenuation));
+		return litColor;
 	}
-	//litColor = attenuation * litColor;
-	//litColor = float3(0,0,0);
-	return litColor;
-	// attenuate
-	//litColor = litColor / dot( float3(0,0.00,0.00005), float3(1.0f, dist, dist*dist) );
-	litColor = litColor / dot( float3(0,0.00,0.000015), float3(1.0f, dist, dist*dist) );
 	
-	//litColor = litColor / dot( light.attenuation, float3(1.0f, dist, dist*dist) );
-	return litColor;
+	[branch]
+	float diffuseIntensity = saturate(dot(normal,lightVec));
+	float attenuation =  energy* 2* (range*range / ( range*range + dist*dist))*(range/(range+5.0*dist));
+	//float intensity = attenuation * ( range - lightVec) / range;
+	float3 h = normalize(lightVec + eyeVec);
+	float averageStrengthSpec = ((surface.specular.r+surface.specular.g+surface.specular.b)/3);
+	float specLighting = pow(saturate(dot(normal, h)), averageStrengthSpec*20);
+
+	float3 diffuse = surface.diffuse.rgb*attenuation*saturate(surface.diffuse.rgb+light.diffuse.rgb)*diffuseIntensity*0.8;
+	float3 specularity = surface.specular.rgb*specLighting*attenuation*light.diffuse.rgb*0.8;
+	return (diffuse+specularity);
 }
 
 float3 spotLight( SurfaceInfo surface, LightInfo light, float3 eyePos, float3 normal, float3 pixelPos )
@@ -161,5 +121,6 @@ float3 spotLight( SurfaceInfo surface, LightInfo light, float3 eyePos, float3 no
 	float s = pow( max( dot(-lightVec, light.lightDir), 0.0f ), light.spotPower );
 
 	// Scale color by spotlight factor.
+	return float3(0,0,0);
 	return litColor*s;
 }
