555ad2a40668829ea3e6878db367c611c5cdd501
Robin Thunström <robin.thunstrom@gmail.com>
2013-02-25 17:15:49 +0100

    Merged ServerPacketHandlerSystem.h

--- Projects/Gameplay/Src/ServerPacketHandlerSystem.h
+++ Projects/Gameplay/Src/ServerPacketHandlerSystem.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#define FORCE_VS_DBG_OUTPUT
+
 #include <map>
 #include <EntitySystem.h>
 #include "ClientInfo.h"
+#include <string>
 
 class TcpServer;
 class PhysicsSystem;
@@ -30,9 +33,16 @@ public:
 	void initialize();
 
 	void processEntities( const vector<Entity*>& p_entities );
+	void createAndBroadCastShip( int p_clientIdentity, int p_playerID );
+
+	void printPacketTypeNotHandled(string p_state, int p_packetType);
+
+private:
+	Entity* createTheShipEntity(int p_newlyConnectedClientId, int p_playerID);
 
 private:
 	TcpServer*		m_server;
 	PhysicsSystem*	m_physics;
 	map<int,ClientInfo> m_clients; ///< The key is the clientID
+	int m_finishedLoadingPlayers;
 };
