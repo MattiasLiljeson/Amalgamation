7c9a434aa84f222175ae72018fbe52d6e50d71a5
Johan Carlberg <johan.carlsberg@gmail.com>
2013-02-27 09:28:43 +0100

    Merge.

---
+++ Projects/Gameplay/Src/AnomalyAcceleratorModule.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <Component.h>
+#include <ComponentFactory.h>
+// =======================================================================================
+// AnomalyAcceleratorModule
+// =======================================================================================
+
+///---------------------------------------------------------------------------------------
+/// \brief A module that accelerates large particles and crashes them together almost at
+/// the speed of light. This in turn give rise to a chaotic pattern in the Higgs field
+/// and creates tiny black holes that lasts for several seconds.
+///        
+/// # AnomalyAcceleratorModule
+/// Detailed description...
+/// Created on: 26-2-2013 
+///---------------------------------------------------------------------------------------
+class AnomalyAcceleratorModule: public Component
+{
+public:
+	AnomalyAcceleratorModule();
+	void init( vector<ComponentData> p_initData );
+
+public:
+	float cooldown;
+
+public:
+	static ComponentRegister<AnomalyAcceleratorModule> s_reg;
+};
---
+++ Projects/Gameplay/Src/AnomalyAcceleratorModuleControllerSystem.cpp
@@ -0,0 +1,84 @@
+#include "AnomalyAcceleratorModuleControllerSystem.h"
+#include "ShipModule.h"
+#include "AnomalyAcceleratorModule.h"
+#include "Transform.h"
+#include "PhysicsBody.h"
+#include "PhysicsSystem.h"
+#include <PhysicsController.h>
+#include "BodyInitData.h"
+#include "NetworkSynced.h"
+#include "LoadMesh.h"
+#include "EntityCreationPacket.h"
+#include <TcpServer.h>
+#include "ModuleHelper.h"
+
+AnomalyAcceleratorModuleControllerSystem::AnomalyAcceleratorModuleControllerSystem(
+	TcpServer* p_server)
+	: EntitySystem(SystemType::AnomalyAcceleratorModuleControllerSystem, 4,
+	ComponentType::AnomalyAcceleratorModule, ComponentType::Transform,
+	ComponentType::PhysicsBody, ComponentType::ShipModule)
+{
+	m_server = p_server;
+}
+
+void AnomalyAcceleratorModuleControllerSystem::processEntities(
+	const vector<Entity*>& p_entities )
+{
+	float dt = m_world->getDelta();
+	for(unsigned int i=0; i<p_entities.size(); i++)
+	{
+		ShipModule* module = static_cast<ShipModule*>(
+			p_entities[i]->getComponent(ComponentType::ShipModule));
+		if(module->isOwned())
+		{
+			AnomalyAcceleratorModule* anomalyAccelerator = static_cast<
+				AnomalyAcceleratorModule*>(p_entities[i]->getComponent(
+				ComponentType::AnomalyAcceleratorModule));
+			anomalyAccelerator->cooldown -= dt;
+			if(anomalyAccelerator->cooldown <= 0.0f && module->getActive())
+			{
+				anomalyAccelerator->cooldown = 1.0f;
+				Transform* transform = static_cast<Transform*>(
+					m_world->getComponentManager()->getComponent(p_entities[i],
+					ComponentType::getTypeFor(ComponentType::Transform)));
+				PhysicsBody* physBody = static_cast<PhysicsBody*>(
+					p_entities[i]->getComponent(ComponentType::PhysicsBody));
+				PhysicsSystem* physics = static_cast<PhysicsSystem*>(
+					m_world->getSystem(SystemType::PhysicsSystem));
+				AglVector3 moduleVelocity = physics->getController()->getBody(
+					physBody->m_id)->GetVelocity();
+				spawnAnomalyBomb(transform, moduleVelocity, module);
+			}
+		}
+	}
+}
+
+void AnomalyAcceleratorModuleControllerSystem::spawnAnomalyBomb( Transform* p_transform,
+	AglVector3 p_moduleVelocity, ShipModule* p_module )
+{
+	Entity* bombEntity = m_world->createEntity();
+	bombEntity->setName("AnomalyBomb");
+	AglVector3 scale = AglVector3(1.0f, 1.0f, 1.0f);
+	Transform* bombTransform = new Transform(p_transform->getTranslation(),
+		p_transform->getRotation(), scale);
+	bombEntity->addComponent(bombTransform);
+	bombEntity->addComponent(new PhysicsBody());
+	bombEntity->addComponent(new BodyInitData(p_transform->getTranslation(),
+		p_transform->getRotation(), scale, p_moduleVelocity,
+		AglVector3::zero(), 0, BodyInitData::DYNAMIC, BodyInitData::SINGLE,
+		false, true));
+	bombEntity->addComponent(new AnomalyBomb());
+	bombEntity->addComponent(new NetworkSynced(bombEntity->getIndex(), -1,
+		EntityType::AnomalyBomb));
+	m_world->addEntity(bombEntity);
+
+	EntityCreationPacket data;
+	data.entityType		= static_cast<char>(EntityType::AnomalyBomb);
+	data.owner			= -1;
+	data.networkIdentity = bombEntity->getIndex();
+	data.translation	= bombTransform->getTranslation();
+	data.rotation		= bombTransform->getRotation();
+	data.scale			= bombTransform->getScale();
+	data.meshInfo		= 1;
+	m_server->broadcastPacket(data.pack());
+}
