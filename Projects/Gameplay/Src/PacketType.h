6003343922a9963ad04094412938964ca85a99cd
Johan Carlberg <johan.carlsberg@gmail.com>
2013-03-11 16:26:52 +0100

    Renamed HighlightSlotPacket's type to be of same name as the struct.

--- Projects/Gameplay/Src/PacketType.h
+++ Projects/Gameplay/Src/PacketType.h
@@ -49,7 +49,7 @@ public:
 		/************************************************************************/
 		ThrustPacket,
 		RayPacket,
-		ModuleHighlightPacket,
+		HighlightSlotPacket,
 		SimpleEvent,
 		ParticleUpdate,
 		ParticleSystemCreationInfo,
