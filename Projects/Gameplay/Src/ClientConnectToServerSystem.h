ded4987fd878f5fac469743964ec6ff4d4d0f72d
Robin Thunström <robin.thunstrom@gmail.com>
2013-03-12 11:24:34 +0100

    Merged s9_issue_26 with sprint 9

--- Projects/Gameplay/Src/ClientConnectToServerSystem.h
+++ Projects/Gameplay/Src/ClientConnectToServerSystem.h
@@ -36,13 +36,10 @@ public:
 	bool setAddressAndConnect(const std::string& p_address, const std::string& p_port);
 	bool setConnectionAddress(const std::string& p_address, const std::string& p_port);
 private:
-
 	bool validateNetworkAddress(const std::string& p_address, const std::string& p_port);
 	bool validateIPFormat(const std::string& p_address);
 	bool validatePortFormat(const std::string& p_port);
-
 private:
-
 	TcpClient* m_tcpClient;
 
 	bool	m_isLookingForConnection;
