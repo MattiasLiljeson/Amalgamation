b90a908ad2e4d9335f94624b9f0432ac486887ee
Johan Carlberg <johan.carlsberg@gmail.com>
2013-03-21 10:00:46 +0100

    F4-cheat is enabled only in cheat-mode.

--- Projects/Gameplay/Src/ClientDebugModuleSpawnerSystem.cpp
+++ Projects/Gameplay/Src/ClientDebugModuleSpawnerSystem.cpp
@@ -4,6 +4,7 @@
 #include "SpawnDebugModulePacket.h"
 #include "EntityType.h"
 #include "Transform.h"
+#include "SettingsSystem.h"
 
 ClientDebugModuleSpawnerSystem::ClientDebugModuleSpawnerSystem(TcpClient* p_client)
 	: EntitySystem(SystemType::ClientDebugModuleSpawnerSystem, 3,
@@ -17,21 +18,25 @@ void ClientDebugModuleSpawnerSystem::processEntities( const vector<Entity*>& p_e
 {
 	for(unsigned int i=0; i<p_entities.size(); i++)
 	{
-		if(m_input->getDeltaByEnum(InputHelper::KeyboardKeys_F4) > 0.0)
+		SettingsSystem* settings = static_cast<SettingsSystem*>(m_world->getSystem(SystemType::SettingsSystem));
+		if(settings->getSettings().enableCheats)
 		{
-			Transform* transform = static_cast<Transform*>(p_entities[i]->getComponent(
-				ComponentType::Transform));
-			SpawnDebugModulePacket data;
-			data.shipPosition = transform->getTranslation();
-			data.moduleTypes[0] = EntityType::ShieldModule;
-			data.moduleTypes[1] = EntityType::AnomalyModule;
-			data.moduleTypes[2] = EntityType::BoosterModule;
-			data.moduleTypes[3] = EntityType::MineLayerModule;
-			data.moduleTypes[4] = EntityType::MinigunModule;
-			data.moduleTypes[5] = EntityType::TeslaCoilModule;
-			data.moduleTypes[6] = EntityType::RocketLauncherModule;
-			data.numberOfModules = 7;
-			m_client->sendPacket(data.pack());
+			if(m_input->getDeltaByEnum(InputHelper::KeyboardKeys_F4) > 0.0)
+			{
+				Transform* transform = static_cast<Transform*>(p_entities[i]->getComponent(
+					ComponentType::Transform));
+				SpawnDebugModulePacket data;
+				data.shipPosition = transform->getTranslation() + AglVector3(10.0f, 0, 0);
+				data.moduleTypes[0] = EntityType::ShieldModule;
+				data.moduleTypes[1] = EntityType::AnomalyModule;
+				data.moduleTypes[2] = EntityType::BoosterModule;
+				data.moduleTypes[3] = EntityType::MineLayerModule;
+				data.moduleTypes[4] = EntityType::MinigunModule;
+				data.moduleTypes[5] = EntityType::TeslaCoilModule;
+				data.moduleTypes[6] = EntityType::RocketLauncherModule;
+				data.numberOfModules = 7;
+				m_client->sendPacket(data.pack());
+			}
 		}
 	}
 }
